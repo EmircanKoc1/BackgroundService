ffddd4a648b1fe7a77524c127ee8cdccbdf7f3d9
Emircan <emircandv787@gmail.com>
2024-05-05 21:59:06 +0300

    Add project files.

---
+++ BackgroundService.API/BackgroundServices/BGLogService.cs
@@ -0,0 +1,37 @@
+﻿
+using meh = Microsoft.Extensions.Hosting;
+
+namespace BackgroundService.API.BackgroundServices
+{
+    public class BGLogService : meh.BackgroundService
+    {
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Started");
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+
+            Console.WriteLine("Stopped");
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                PrintConsole();
+                await Task.Delay(1000);
+            }
+
+        }
+
+        void PrintConsole() => Console.WriteLine("exetuing");
+
+    }
+}
